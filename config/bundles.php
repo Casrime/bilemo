9d704be50136e09d699023d777f3bcc3720036d2
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-06-02 16:10:54 +0200

    Update docker configuration

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
